34b3afd19e455f102abe4581b09fd185f1af68d3
juan-lukeEtapath <juan@etapath.com>
2023-10-18 09:19:13 +0200

    feat: Implement all components and add basic styling

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
+  const [goals, setGoals] = useState([]);
+  const [visible, setVisible] = useState(false);
+
+  function startAddGoalHandler() {
+    setVisible(true);
+  };
+
+  function endAddGoalHandler() {
+    setVisible(false);
+  };
+
+  function addGoalHandler(enteredGoalText) {
+    setGoals((goalsArray) => [...goalsArray, {text: enteredGoalText, uuid: Math.random().toString() }]);
+    endAddGoalHandler();
+  };
+
+  function deleteGoalHandler(uuid) {
+    setGoals((goalsArray) => {
+      return goalsArray.filter((goal) => goal.uuid !== uuid);
+    });
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="Add new goals" color='#5E0ACC' onPress={startAddGoalHandler} />
+    {visible && <Text style={styles.heading}>Hello World!</Text> &&  <GoalInput onAddGoal={addGoalHandler} onCancel={endAddGoalHandler}/>}
+      <View style={styles.goalsContainer}>
+        <FlatList data={goals} renderItem={(itemData) => {
+          return <GoalItem text={itemData.item.text} uuid={itemData.item.uuid} onDeleteGoal={deleteGoalHandler}/>;
+        }}
+        keyExtractor={(item, index) => { return item.uuid}}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    padding: 50,
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  goalsContainer: {
+    flex: 3,
+    padding: 4,
+    justifyContent: 'space-evenly',
+  },
+  heading: {
+    fontSize: 42,
+    color: '#000000',
+    marginBottom: 8
   },
 });
